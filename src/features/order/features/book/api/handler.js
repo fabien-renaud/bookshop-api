649b1d0f3fa88d00229a7275558184ba871aebcb
fabien_renaud <frd.frenaud@gmail.com>
2021-03-19 00:14:17 +0100

    Fix handler payload

--- src/features/order/features/book/api/handler.js
+++ src/features/order/features/book/api/handler.js
@@ -3,8 +3,8 @@ import OrderBookController from '../controller';
 
 const addBook = (req, h) => {
     const {orderId, bookId} = req.params;
-    const payload = req.payload;
-    return OrderBookController.addBook({orderId, bookId, ...payload})
+    const {quantity} = req.payload;
+    return OrderBookController.addBook({orderId, bookId, quantity})
         .then(() => h.response().code(201))
         .catch(() => conflict());
 };
